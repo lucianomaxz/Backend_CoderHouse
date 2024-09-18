c92bfc1a82ea09420c853c216eae6a3d67c1ddac
Canteros Luciano <lucianomaxz2000@gmail.com>
2024-09-18 18:48:37 -0300

    Entrega final

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,34 +1,18 @@
 import { Router } from "express";
+import { UserModel } from "../daos/mongodb/models/user.model.js"  // Importar el modelo de usuario
 const router = Router();
-// import {
-//   login,
-//   logout,
-//   visit,
-//   infoSession,
-//   register,
-// } from "../controllers/user.controller.js";
-import { validateLogin } from "../middlewares/validateLogin.js";
 
 import passport from "passport";
-import { isAuth } from "../middlewares/isAuth.js";
 import { checkAuth } from "../middlewares/jwt.js";
 import { checkAdmin } from "../middlewares/checkAdmin.js";
 
 import UserController from '../controllers/user.controller.js';
 const controller = new UserController();
 
-// router.post("/register", passport.authenticate('register'), registerResponse);
-// router.post('/register', register)
-
-// router.post('/login', passport.authenticate('login'), loginResponse);
-
-/* ------------------------------------ - ----------------------------------- */
 
 
 //!  BOTON: | INICIAR SESION CON GITHUB |
 router.get('/register-github', passport.authenticate('github', { scope: [ 'user:email' ] }));
-
-//router.get('/profile', passport.authenticate('github', { scope: [ 'user:email' ] }), githubResponse)
 
 
 router.post('/register', controller.register);
@@ -36,14 +20,18 @@ router.post('/register', controller.register);
 router.post('/login', controller.login);
 
 router.get('/profile', checkAuth, controller.profile);
-// router.get('/api/session/', passport.authenticate('github', { scope: [ 'user:email' ] }), githubResponse)
 
-// router.get('/profile', passport.authenticate( 'github' , {
-//     failureRedirect: '/login', 
-//     successRedirect: '/profile-github', 
-//     passReqToCallback: true
-// }));
-
+// Endpoint para obtener todos los usuarios 
+router.get('/', async (req, res) => {
+  try {
+    
+    const users = await UserModel.find().select('first_name email role');
+    
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ message: 'Error al obtener usuarios', error });
+  }
+});
 router.get('/logout', (req, res) => {
     req.logout((err) => {
         if (err) res.send(err);
@@ -53,15 +41,9 @@ router.get('/logout', (req, res) => {
 
 
 
-// router.post("/login", login);
-// router.post('/login', loginJwt);
-// router.get("/info", validateLogin, infoSession);
-
 router.get('/private', checkAuth, (req, res)=>res.json({ user: req.user }));
 
 router.get('/', checkAdmin, controller.checkUsersLastConnection);
 
-// router.get("/secret-endpoint", validateLogin, visit);
-// router.post("/logout", logout);
 
 export default router;
